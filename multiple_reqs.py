d183f49daa368333aebc5afafddf2bce946ce9ec
eugen <eugen.yzeiri@gmail.com>
2019-01-26 16:15:04 +0100

    add: ask for user input; build first user flow;

--- multiple_reqs.py
+++ multiple_reqs.py
@@ -2,13 +2,61 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-keys = ['mish', 'peshk', 'djathe']
+# CONST's
+example_keys = ['mish', 'peshk', 'djathe']
 headers = ['Nr', 'Kodi', '11 Shifrori', 'Pershkrimi', 'Tarifa baze', 'TVSH', 'EFTA', 'CEFTA', 'BE', 'Turqi']
 
-for key in keys:
-    r = requests.post('http://www.dogana.gov.al/preferenca/fetch.php', data={'keyword': key})
-    soup = BeautifulSoup(r.text)
-    with open(key + '_responses.csv', 'w') as f:
-        wr = csv.writer(f)
-        wr.writerow(headers)
-        wr.writerows([[td.text.encode('utf-8') for td in row.find_all('td')] for row in soup.select('tr + tr')])
+
+"""Entry point: greet the user and ask for the querying method"""
+user_screen = input('Hey. Press 1 to type your keywords. Press 2 to import your keywords from a CSV file. Press 3 to run the demo.\n')
+user_response = int(user_screen)
+
+
+def typed_by_user():
+    """Method to handle user input keywords and query API"""
+    input_keys = input('Enter your keywords separated by a comma and a space: ')
+    chosen_keys = input_keys.split(', ')
+    for key in chosen_keys:
+        r = requests.post('http://www.dogana.gov.al/preferenca/fetch.php', data={'keyword': key})
+        soup = BeautifulSoup(r.text)
+        with open(key + '_responses.csv', 'w') as f:
+            wr = csv.writer(f)
+            wr.writerow(headers)
+            wr.writerows([[td.text.encode('utf-8') for td in row.find_all('td')] for row in soup.select('tr + tr')])
+
+
+def imported_from_file():
+    """Method to handle file input keywords and query API"""
+    chosen_keys = []
+    print('Make sure your CSV file is called "keys" has one single row with the specified keywords in it!')
+    file = open('keys.csv', 'r')
+    for line in file:
+        temp = line.split(',')
+        for t in temp:
+            chosen_keys.append(t)
+    print('keys: ', chosen_keys)
+    for key in chosen_keys:
+        r = requests.post('http://www.dogana.gov.al/preferenca/fetch.php', data={'keyword': key})
+        soup = BeautifulSoup(r.text)
+        with open(key + '_responses_from_file.csv', 'w') as f:
+            wr = csv.writer(f)
+            wr.writerow(headers)
+            wr.writerows([[td.text.encode('utf-8') for td in row.find_all('td')] for row in soup.select('tr + tr')])
+
+
+def run_demo():
+    for key in example_keys:
+        r = requests.post('http://www.dogana.gov.al/preferenca/fetch.php', data={'keyword': key})
+        soup = BeautifulSoup(r.text)
+        with open(key + '_example_responses.csv', 'w') as f:
+            wr = csv.writer(f)
+            wr.writerow(headers)
+            wr.writerows([[td.text.encode('utf-8') for td in row.find_all('td')] for row in soup.select('tr + tr')])
+
+
+if user_response == 1:
+    typed_by_user()
+elif user_response == 2:
+    imported_from_file()
+else:
+    run_demo()
